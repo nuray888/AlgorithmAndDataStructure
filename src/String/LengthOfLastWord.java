2583bfef73fd962988228f760a654f8ad5933fe2
nuray888 <nuraymuxtarli67@gmail.com>
2025-01-24 13:38:08 +0400

    Easy

---
+++ src/String/LengthOfLastWord.java
@@ -0,0 +1,16 @@
+package String;
+
+public class LengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+        //! We use this method for to get rid of empty string that at the end and at the beginning
+        s = s.trim();
+        int count = 0; //we count the length of the last word
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' ') {
+                break;
+            }
+            count++;
+        }
+        return count;
+    }
+}
